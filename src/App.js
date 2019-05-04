fb6bf4a861b277d112828977cc19cc8204fcdfa5
David Svetlecic <DSvetlecic@cylance.com>
2019-05-04 01:33:06 -0500

    Works with DefineMessages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { injectIntl, defineMessages } from "react-intl";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = defineMessages({
+  title: {
+    id: 'app.title',
+    defaultMessage: 'Welcome to React'
+  },
+  content1: {
+    id: 'app.content1',
+    defaultMessage: 'To get started, edit'
+  },
+  content2: {
+    id: 'app.content2',
+    defaultMessage: 'and save to reload.'
+  },
+})
+
+class App extends Component {
+  render() {
+    const {intl:{formatMessage}} = this.props;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="languages">
+              <a href="/?locale=fr">Français</a>
+              <a href="/?locale=en">English</a>
+              <a href="/?locale=es">Español</a>
+            </div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">{formatMessage(messages.title)}</h1>
+          </header>
+          <p className="App-intro">
+            {formatMessage(messages.content1)} <code>src/App.js</code> {formatMessage(messages.content2)}
+          </p>
+        </div>
+    );
+  }
 }
 
-export default App;
+export default injectIntl(App);
